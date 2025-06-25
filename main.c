893f9b84b6ee235c919b1f4172dd3bd6914d9be9
Wendy <alilundqvist2@gmail.com>
2025-06-25 21:08:25 +0200

    First Try

--- main.c
+++ main.c
@@ -5,17 +5,13 @@ int main()
 {
     // The Line below must *NOT* be changed or test will fail!
     srand(1);
-
-   
-    // Your code goes here
         int number_storage[100];
         int random_number;
         int number_occurences[6] = {0, 0, 0, 0, 0, 0}; // Array to count occurrences of numbers 1-6
         int sum = 0;
-for (int i = 0; i < 100;)
+for (int i = 0; i < 100; i++)
 {
-    random_number = rand() % 6 + 1; // Generate a random number between 1 and 6
-    printf("%d\n", random_number);
+    random_number = (rand() % 6) + 1; // Generate a random number between 1 and 6
     number_storage[i] = random_number;
     sum += random_number; // Add the random number to the sum
     if (number_storage[i] == 1) {
@@ -31,15 +27,17 @@ for (int i = 0; i < 100;)
     } else if (number_storage[i] == 6) {
         number_occurences[5]++;
     }
-    i++;
 }
 
-printf("Occurrences of numbers 1-6:\n");
+
 for (int i = 0; i < 6; i++) {
-    printf("Number %d: %d times\n", i + 1, number_occurences[i]);
+    printf("%d\n", number_occurences[i]);
 }
-printf("Sum of all random numbers: %d\n", sum);
+printf("%d\n", sum);
 float average = sum / 100.0; // Calculate the average of the random numbers
-printf("Average of random numbers: %.1f\n", average);
+printf("%.1f\n", average);
     return 0;
+   
+    // Your code goes here
+
 }
